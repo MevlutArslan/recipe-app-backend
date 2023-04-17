18ed9a987741c74ac67f26031b88ad3b9e9dd9d8
MevlutArslan <mevlut12098@gmail.com>
2023-04-17 01:28:51 -0500

    implement database clearing at start

--- src/db.py
+++ src/db.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
+from models.base import Base
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = "postgresql://mvlcfr:cafer12.@localhost/recipe_app"
+SQLALCHEMY_DATABASE_URL = "postgresql://mvlcfr:cafer12.@localhost/recipe_app?client_encoding=utf8"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Drop all tables
+Base.metadata.drop_all(bind=engine)
+
+# Recreate all tables
+Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(engine)
 session = Session()
