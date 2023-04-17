b6397ca17cc505eedfd550880219823c4986f95d
MevlutArslan <mevlut12098@gmail.com>
2023-04-17 01:06:24 -0500

    use session instead of connection

--- src/db.py
+++ src/db.py
@@ -1,13 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 SQLALCHEMY_DATABASE_URL = "postgresql://mvlcfr:cafer12.@localhost/recipe_app"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-try:
-    conn = engine.connect()
-    print("Connection established successfully!")
-    conn.close()
-except OperationalError as e:
-    print(f"The error '{e}' occurred.")
+Session = sessionmaker(engine)
+session = Session()
