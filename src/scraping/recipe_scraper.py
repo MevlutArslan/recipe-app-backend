a2c5f7f98e05e5467106e8a5884752d694df3ddd
MevlutArslan <mevlut12098@gmail.com>
2023-05-01 19:05:16 -0500

    scrape for the new fields on Recipe table

--- src/scraping/recipe_scraper.py
+++ src/scraping/recipe_scraper.py
@@ -7,6 +7,19 @@ class NYT_Scraper(BaseScraper):
     def scrape_name(self):
         self._name = self.scraper.find("h1", class_="recipe-name").text
 
+    def scrape_imageUrl(self):
+        img_tag = self.scraper.select_one('.recipe-single-img img')
+        self._imageUrl = img_tag['src']
+
+    def scrape_yield(self):
+        self._recipeYield = self.scraper.find('span', itemprop="recipeYield").get_text()
+
+    def scrape_prepTime(self):
+        self._prepTime = self.scraper.find('span', {'itemprop': 'prepTime'}).text.strip()
+
+    def scrape_cookTime(self):
+        self._cookTime = self.scraper.find('span', {'itemprop': 'cookTime'}).text.strip()
+    
     def scrape_ingredients(self):
         ingredients_div = self.scraper.find("div", class_="recipe-materials-div")
         for p_tag in ingredients_div.findAll("p", class_=""):
--- src/scraping/scraper.py
+++ src/scraping/scraper.py
@@ -4,7 +4,11 @@ class BaseScraper:
     def __init__(self, url):
         self.url = url
         self._name: str
+        self._imageUrl: str
         self._author_name: str
+        self._recipeYield: str
+        self._prepTime: str
+        self._cookTime: str
         self._ingredients = []
         self._instructions = []
         self.scraper: BeautifulSoup
@@ -18,6 +22,10 @@ class BaseScraper:
         self.get_html()
         self.scrape_author()
         self.scrape_name()
+        self.scrape_imageUrl()
+        self.scrape_yield()
+        self.scrape_prepTime()
+        self.scrape_cookTime()
         self.scrape_ingredients()
         self.scrape_instructions()
     
@@ -25,6 +33,18 @@ class BaseScraper:
         raise NotImplementedError
     
     def scrape_name():
+        raise NotImplementedError
+    
+    def scrape_imageUrl():
+        raise NotImplementedError
+    
+    def scrape_prepTime():
+        raise NotImplementedError
+    
+    def scrape_cookTime():
+        raise NotImplementedError
+    
+    def scrape_yield():
         raise NotImplementedError
     
     def scrape_ingredients(self):
