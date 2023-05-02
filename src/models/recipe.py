ce05eb04fe5745a841d7c269f52b1d17e55d23f8
MevlutArslan <mevlut12098@gmail.com>
2023-05-01 19:05:05 -0500

    add additional fields to Recipe table

--- src/models/recipe.py
+++ src/models/recipe.py
@@ -8,6 +8,10 @@ class Recipe(Base):
     __tablename__ = 'recipes'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String)
+    image_url = Column("imageUrl", String)
     author_id = Column(UUID(as_uuid=True), ForeignKey('authors.id'))
+    recipe_yield = Column("yield", String)
+    prepTime = Column("prepTime", String)
+    cookTime = Column("cookTime", String)
     ingredients = Column("ingredients", ARRAY(JSON))
     instructions = Column("instructions", ARRAY(String))
